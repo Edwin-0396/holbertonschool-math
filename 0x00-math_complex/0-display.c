98d2913933226dc286f8a43fe3feb0732a96c37e
Edwin-9603 <egc4611@gmail.com>
2022-04-27 10:25:50 -0500

    New math

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 /**
- * display_complex_number
- * 
- * @c:  
+ * display_complex_number - function that displays
+ * the complex numbers, followed by a new line.
+ *
+ * @c: structure “complex” where a complex number
+ * a + ib is represented by two doubles.
  */
 
 void display_complex_number(complex c)
@@ -14,6 +16,5 @@ void display_complex_number(complex c)
 	if (c.re != 0 && c.im == 0)
 		printf("%d\n", c.re);
 	if (c.re == 0 && c.im != 0)
-		printf("%di\n",c.im);
-	
+		printf("%di\n", c.im);
 }
